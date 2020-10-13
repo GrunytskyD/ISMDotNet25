76200c826bff24c66e215d54027c06c6d66db434
Dmytro Grunytsky <dima.grunitsky@gmail.com>
2020-10-13 22:54:36 +0300

    назви дня тижня

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++ ) 
+            {
+                if (i == 5)
+                    continue;
+                Console.WriteLine($"Понеділок,івторок,середа,четверг,п'ятниця,субота,неділя{i}");
+
+            }                
+        }
+    }
+}
